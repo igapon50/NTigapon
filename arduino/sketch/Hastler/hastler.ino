8503c33192d9ae19e42997c729a3fbf658ba5089
igapon <igapon@gmail.com>
2017-12-09 00:07:54 +0900

    コード整形、タイムアウトチェック処理修正、モーター制御の入力補正

--- arduino/sketch/Hastler/hastler.ino
+++ arduino/sketch/Hastler/hastler.ino
@@ -1,13 +1,7 @@
-
 #include <Wii.h>
 #include <usbhub.h>
 #include <Wire.h>
 #include "hastlermotor.h"
-
-#define Hastler_front ONE 
-#define Hastler_back TWO
-#define Hastler_right DOWN
-#define Hastler_left UP
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
 #ifdef dobogusinclude
@@ -16,17 +10,15 @@
 #endif
 
 USB Usb;
-//USBHub Hub1(&Usb); // Some dongles have a hub inside
+BTD Btd(&Usb);
+WII Wii(&Btd, PAIR);
 
-BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
-/* You can create the instance of the class in two ways */
-WII Wii(&Btd, PAIR); // This will start an inquiry and then pair with your Wiimote - you only have to do this once
-//WII Wii(&Btd); // After that you can simply create the instance like so and then press any button on the Wiimote
-
-bool printAngle;
-byte x = 0;
-unsigned int checkcount = 0;
+#define VERSION_STRING "0.0.4"
 #define CONNECTION_TIMEOUT (50/*タイムアウト判断するチェック回数*/)
+#define Hastler_front ONE 
+#define Hastler_back TWO
+#define Hastler_right DOWN
+#define Hastler_left UP
 float older_pitch = 0.0;//切断検出用
 float older_roll = 0.0;//切断検出用
 const int sensor_light = A0;
@@ -35,6 +27,7 @@ const int back_light = A2;
 
 void setup() {
   Serial.begin(115200);
+  Serial.print(F(__DATE__ "/" __TIME__ "/" VERSION_STRING));
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
@@ -62,53 +55,59 @@ void loop() {
         analogWrite(back_light, 0);
       }
       Wii.disconnect();
-    } else {
-      float newer_pitch,newer_roll;
-      newer_pitch = Wii.getPitch();
-      newer_roll = Wii.getRoll();
-      if( older_pitch == newer_pitch && older_roll == newer_roll ){
-        checkcount++;
-      }else{
-        older_pitch = newer_pitch;
-        older_roll = newer_roll;
-        checkcount = 0;
-      }
-      if (CONNECTION_TIMEOUT < checkcount){
-        checkcount = 0;
-        {
-          //接続が切れたとき動作を停止する
-          hastler_moter_front(STOP_MOTER_VAL);
-          hastler_moter_back(STOP_MOTER_VAL);
-          analogWrite(front_light, 0);
-          analogWrite(back_light, 0);
+    }else{
+      if(val_back != STOP_MOTER_VAL){ //駆動時だけチェックする
+        static unsigned int checkcount = 0;
+        float newer_pitch,newer_roll;
+        newer_pitch = Wii.getPitch();
+        newer_roll = Wii.getRoll();
+        if(older_pitch == newer_pitch && older_roll == newer_roll){ //値に変化なし
+          checkcount++;
+          if(CONNECTION_TIMEOUT < checkcount){ //規定回数に達したら切断処理する
+            checkcount = 0;
+            {
+              //接続が切れたとき動作を停止する
+              hastler_moter_front(STOP_MOTER_VAL);
+              hastler_moter_back(STOP_MOTER_VAL);
+              analogWrite(front_light, 0);
+              analogWrite(back_light, 0);
+            }
+            Wii.disconnect();
+            return;
+          }
+        }else{ //値に変化あり
+          older_pitch = newer_pitch;
+          older_roll = newer_roll;
+          checkcount = 0;
         }
-        Wii.disconnect();
-        return;
+      }else{ //停止時はノーチェックでカウントをクリア
+        checkcount = 0;
       }
-      if (Wii.getButtonPress(Hastler_left)) {
+
+      if(Wii.getButtonPress(Hastler_left)){ //ステアリング制御左
         Serial.print(F("\r\nLeft"));
         hastler_moter_front(MIN_MOTER_VAL);
-      }else if (Wii.getButtonPress(Hastler_right)) {
+      }else if(Wii.getButtonPress(Hastler_right)){ //ステアリング制御右
         Serial.print(F("\r\nRight"));
         hastler_moter_front(MAX_MOTER_VAL);
-      }else{
+      }else{ //ステアリング制御中立
         Serial.print(F("\r\n"));
         hastler_moter_front(STOP_MOTER_VAL);
       }
 
-      if (Wii.getButtonPress(Hastler_back)) {
+      if(Wii.getButtonPress(Hastler_back)){ //駆動制御減速
         Serial.print(F("\r\nDown"));
         analogWrite(back_light, 0);
         if(0 < val_back){
           hastler_moter_back(--val_back);
         }
-      }else if (Wii.getButtonPress(Hastler_front)) {
+      }else if(Wii.getButtonPress(Hastler_front)){ //駆動制御加速
         Serial.print(F("\r\nUp"));
         analogWrite(back_light, 0);
         if(val_back < MAX_MOTER_VAL){
           hastler_moter_back(++val_back);
         }
-      }else{
+      }else{ //駆動制御中立
         Serial.print(F("\r\n"));
         analogWrite(back_light, 255);
         if(val_back <= STOP_MOTER_VAL){
@@ -118,15 +117,12 @@ void loop() {
         }
       }
 
-      if (Wii.getButtonClick(A)) {
-        printAngle = !printAngle;
+      if(Wii.getButtonClick(A)){
         Serial.print(F("\r\nA"));
         analogWrite(front_light, 255);
         analogWrite(back_light, 255);
         Wire.beginTransmission(8); // transmit to device #8
-//        Wire.write("x is ");        // sends five bytes
-//        Wire.write(x);              // sends one byte
-        Wire.write('A');              // sends one byte
+        Wire.write('A');           // sends one byte
         Wire.endTransmission();    // stop transmitting
       }else{
         int sensorValue = analogRead(sensor_light);
