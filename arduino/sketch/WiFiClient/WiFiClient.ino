046f5ed0579d732b7ec7a14734e628f30e725da6
igapon <igapon@gmail.com>
2018-01-22 22:53:27 +0900

    ESP32 Dev Module - arduinoIDEでRICOH THETAのWebAPI制御 - 静止画撮影

---
+++ arduino/sketch/WiFiClient/WiFiClient.ino
@@ -0,0 +1,85 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <WiFi.h>
+#include "thetav2_1.h"
+
+const char* ssid     = "THETAYL00101594.OSC";
+const char* password = "00101594";
+
+const char* host = WEB_SERVER;
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+ char buffer[1024] = {'\0'};
+ const char *requests[] = {
+    REQUEST_MAIN_takePicture,
+    NULL
+  };
+
+    delay(5000);
+
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+  for(int no = 0 ; requests[no] != NULL ; no++){
+    margeString_POST_Request(buffer, 1024, requests[no]);
+    // This will send the request to the server
+    client.print(String(buffer));
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+  }
+    Serial.println();
+    Serial.println("closing connection");
+}
+
