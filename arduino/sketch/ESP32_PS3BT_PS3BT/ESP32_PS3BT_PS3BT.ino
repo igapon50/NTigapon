061f308c7eb0b40a26c61c99a7c6fb04c006dae1
igarashi <ryosuke.igarashi@jp.ricoh.com>
2018-07-26 20:08:48 +0900

    スティック処理をPS3コントローラスケッチにも追加

--- arduino/sketch/ESP32_PS3BT_PS3BT/ESP32_PS3BT_PS3BT.ino
+++ arduino/sketch/ESP32_PS3BT_PS3BT/ESP32_PS3BT_PS3BT.ino
@@ -12,14 +12,22 @@
 #include <spi4teensy3.h>
 #endif
 #include <SPI.h>
+#include <Wire.h>
+//   A4(SDA)とA5(SCL)は、I2C通信に使用する
 
 USB Usb;
 //USBHub Hub1(&Usb); // Some dongles have a hub inside
 
 BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 /* You can create the instance of the class in two ways */
-PS3BT PS3(&Btd); // This will just create the instance
-//PS3BT PS3(&Btd, 0x00, 0x15, 0x83, 0x3D, 0x0A, 0x57); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
+//PS3BT PS3(&Btd); // This will just create the instance
+PS3BT PS3(&Btd, 0x00, 0x1B, 0xDC, 0x08, 0xB9, 0xA7); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
+
+//#define SILENT /* 有効にするとシリアルに出力しない */
+#ifndef SILENT
+// SILENTが有効なさいに消したい処理をここに書く
+#endif
+#define VERSION_STRING "0.0.1"
 
 bool printTemperature, printAngle;
 
@@ -38,6 +46,7 @@ void loop() {
   Usb.Task();
 
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
+#if 0
     if (PS3.getAnalogHat(LeftHatX) > 137 || PS3.getAnalogHat(LeftHatX) < 117 || PS3.getAnalogHat(LeftHatY) > 137 || PS3.getAnalogHat(LeftHatY) < 117 || PS3.getAnalogHat(RightHatX) > 137 || PS3.getAnalogHat(RightHatX) < 117 || PS3.getAnalogHat(RightHatY) > 137 || PS3.getAnalogHat(RightHatY) < 117) {
       Serial.print(F("\r\nLeftHatX: "));
       Serial.print(PS3.getAnalogHat(LeftHatX));
@@ -60,6 +69,52 @@ void loop() {
         Serial.print(PS3.getAnalogButton(R2));
       }
     }
+#endif
+
+    static char older_L_X = 'H';
+    static char older_L_Y = 'H';
+    static char older_R_X = 'H';
+    static char older_R_Y = 'H';
+    uint8_t L_X = PS3.getAnalogHat(LeftHatX);
+    uint8_t L_Y = PS3.getAnalogHat(LeftHatY);
+    uint8_t R_X = PS3.getAnalogHat(RightHatX);
+    uint8_t R_Y = PS3.getAnalogHat(RightHatY);
+    if(older_L_X != 'H' || older_L_Y != 'H' || older_R_X != 'H' || older_R_Y != 'H' || L_X > 137 || L_X < 117 || L_Y > 137 || L_Y < 117 || R_X > 137 || R_X < 117 || R_Y > 137 || R_Y < 117){
+      static char newer_L_X = 'H';
+      static char newer_L_Y = 'H';
+      static char newer_R_X = 'H';
+      static char newer_R_Y = 'H';
+      newer_L_X = 'A' + map(L_X,0,255,0,15);
+      newer_L_Y = 'A' + map(L_Y,0,255,0,15);
+      newer_R_X = 'A' + map(R_X,0,255,0,15);
+      newer_R_Y = 'A' + map(R_Y,0,255,0,15);
+      if(older_L_X != newer_L_X || older_L_Y != newer_L_Y || older_R_X != newer_R_X || older_R_Y != newer_R_Y){
+        static char c = 'H';
+        Wire.beginTransmission(8); // transmit to device #8
+        c = older_L_X = newer_L_X;
+        Wire.write(c);
+#ifndef SILENT
+        Serial.print(c);
+#endif
+        c = older_L_Y = newer_L_Y;
+        Wire.write(c);
+#ifndef SILENT
+        Serial.print(c);
+#endif
+        c = older_R_X = newer_R_X;
+        Wire.write(c);
+#ifndef SILENT
+        Serial.print(c);
+#endif
+        c = older_R_Y = newer_R_Y;
+        Wire.write(c);
+#ifndef SILENT
+        Serial.print(c);
+        Serial.print(F("\r\n"));
+#endif
+        Wire.endTransmission();    // stop transmitting
+      }
+    }
 
     if (PS3.getButtonClick(PS)) {
       Serial.print(F("\r\nPS"));
@@ -68,11 +123,11 @@ void loop() {
     else {
       if (PS3.getButtonClick(TRIANGLE)) {
         Serial.print(F("\r\nTraingle"));
-        PS3.setRumbleOn(RumbleLow);
+//        PS3.setRumbleOn(RumbleLow);
       }
       if (PS3.getButtonClick(CIRCLE)) {
         Serial.print(F("\r\nCircle"));
-        PS3.setRumbleOn(RumbleHigh);
+//        PS3.setRumbleOn(RumbleHigh);
       }
       if (PS3.getButtonClick(CROSS))
         Serial.print(F("\r\nCross"));
