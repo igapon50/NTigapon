65b706979265cd9e5ccc104b660c570adad56745
igarashi <ryosuke.igarashi@jp.ricoh.com>
2018-09-06 19:29:23 +0900

    ハードウェアシリアル(arduino USB)<->ソフトウェアシリアル(IM920)

--- arduino/sketch/im920_serial_tx/im920_serial_tx.ino
+++ arduino/sketch/im920_serial_tx/im920_serial_tx.ino
@@ -3,12 +3,27 @@
 SoftwareSerial IM920Serial(8, 9); // ソフトウエアシリアル
 int vol=0, vol_new=0, diff, busy;
 
+//#define SILENT /* 有効にするとシリアルに出力しない */
+#ifndef SILENT
+// SILENTが有効なさいに消したい処理をここに書く
+#endif
+#define VERSION_STRING "0.0.1"
+
 void setup() {
- IM920Serial.begin(19200); // ソフトウエアシリアル 初期化
- pinMode(10, INPUT); // Busy 信号 Pin10 入力
+  Serial.begin(115200);
+  while(!Serial);// シリアルポートの準備ができるのを待つ(Leonardoのみ必要)
+  Serial.print(F(__DATE__ "/" __TIME__ "\r\n"));
+  Serial.print(F(__FILE__ "/" VERSION_STRING "\r\n"));
+
+  IM920Serial.begin(19200); // ソフトウエアシリアル 初期化
+  pinMode(10, INPUT); // Busy 信号 Pin10 入力
 }
 
 void loop() {
+#if 1
+  if (IM920Serial.available()) Serial.write(IM920Serial.read());
+  if (Serial.available()) IM920Serial.write(Serial.read());
+#else
  vol_new = analogRead(0)/4; // Analog_0 データ取得 8bit に変換
  diff = abs(vol_new - vol); // 前回データと比較 差分を diff 変数に
  if (diff > 1) { // アナログ値に変化あれば IM920 から送信
@@ -21,5 +36,6 @@ void loop() {
  vol = vol_new;
  delay(30);
  }
+#endif
 }
 
