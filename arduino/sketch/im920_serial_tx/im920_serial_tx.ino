c811301cabbf5f3b1bdf82574f3c3b72330ebd40
igarashi <ryosuke.igarashi@jp.ricoh.com>
2018-09-14 12:48:12 +0900

    wireを追加

--- arduino/sketch/im920_serial_tx/im920_serial_tx.ino
+++ arduino/sketch/im920_serial_tx/im920_serial_tx.ino
@@ -1,9 +1,16 @@
 /* IM920 DemoProgram */
 #include <SoftwareSerial.h>
 SoftwareSerial IM920Serial(8, 9); // ソフトウエアシリアル
+#define BUSY_PIN 10
 int vol=0, vol_new=0, diff, busy;
 
+#define Enable_I2C
 //#define SILENT /* 有効にするとシリアルに出力しない */
+
+#ifdef Enable_I2C
+#include <Wire.h>
+//   A4(SDA)とA5(SCL)は、I2C通信に使用する
+#endif// Enable_I2C
 #ifndef SILENT
 // SILENTが有効なさいに消したい処理をここに書く
 #endif
@@ -15,14 +22,27 @@ void setup() {
   Serial.print(F(__DATE__ "/" __TIME__ "\r\n"));
   Serial.print(F(__FILE__ "/" VERSION_STRING "\r\n"));
 
+#ifdef Enable_I2C
+  Wire.begin(); // join i2c bus (address optional for master)
+#endif// Enable_I2C
   IM920Serial.begin(19200); // ソフトウエアシリアル 初期化
-  pinMode(10, INPUT); // Busy 信号 Pin10 入力
+  pinMode(BUSY_PIN, INPUT); // Busy 信号 Pin10 入力
 }
 
 void loop() {
 #if 1
-  if (IM920Serial.available()) Serial.write(IM920Serial.read());
-  if (Serial.available()) IM920Serial.write(Serial.read());
+  if(IM920Serial.available()){
+    static char c = 'H';
+    c = IM920Serial.read();
+    Serial.write(c);
+#ifdef Enable_I2C
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(c);
+    Wire.endTransmission();    // stop transmitting
+#endif// Enable_I2C
+  }
+  if(Serial.available()) IM920Serial.write(Serial.read());
+
 #else
  vol_new = analogRead(0)/4; // Analog_0 データ取得 8bit に変換
  diff = abs(vol_new - vol); // 前回データと比較 差分を diff 変数に
